352d0fe02a55c0fa2a63c8ca24d0990b8bebff84
Roman Linkov <roman.linkov@vk.com>
2023-02-12 23:19:36 +0300

    Выполнена задача 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите координаты точки А последовательно Xa Ya Za:");
+int Xa=int.Parse(ReadLine());
+int Ya=int.Parse(ReadLine());
+int Za=int.Parse(ReadLine());
+
+WriteLine("Введите координаты точки B последовательно Xa Ya Za:");
+int Xb=int.Parse(ReadLine());
+int Yb=int.Parse(ReadLine());
+int Zb=int.Parse(ReadLine());
+
+int SquareDiff(int x1, int x2)
+{
+    int SQ = (x2 - x1)*(x2 - x1);
+    return SQ;
+}
+
+double Distance = Math.Sqrt(SquareDiff(Xa, Xb) + SquareDiff(Ya, Yb) + SquareDiff(Za, Zb));
+Distance = Math.Round(Distance, 2);
+
+WriteLine($"Расстояние между A и B = {Distance}");
