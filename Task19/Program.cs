60ff1ac7e01899cab78ee70157cee5983087cd73
Roman Linkov <roman.linkov@vk.com>
2023-02-14 21:29:39 +0300

    Начал палинром

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число N:");
+String Number1=ReadLine();
+int length=Number1.Length;
+
+int[] array = new int[length];
+int i=0;
+WriteLine($"длина {length}");
+WriteLine(Number1[2]);
+
+int k = int.Parse(Number1[1]);
+WriteLine(k);
+
