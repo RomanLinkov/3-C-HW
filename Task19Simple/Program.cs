3101f6314dbaf21fa9f010fa9bbb7ec005682faa
Roman Linkov <roman.linkov@vk.com>
2023-02-15 00:52:16 +0300

    Предложено упрощённое решение задачи 19

---
+++ Task19Simple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+String number1=ReadLine();
+int length=number1.Length;
+
+int i=0;
+int b=length/2;
+bool w=true;
+while (i<b)
+{
+    if (number1[i] != number1[length-1-i]) 
+    {
+        Write("Введённое число не является палиндромом "); 
+        w=false; 
+        break;
+    } 
+    i++;
+} 
+if (w) WriteLine("Введенное число является палиндромом");
+WriteLine();
