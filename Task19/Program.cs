81aa74668100309170d5e15bab39123ab7386fd5
Roman Linkov <roman.linkov@vk.com>
2023-02-15 00:04:09 +0300

    Сделал задачу с палиндромом

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,15 +3,34 @@ using static System.Console;
 
 Clear();
 
-WriteLine("Введите число N:");
+WriteLine("Введите число: ");
 String Number1=ReadLine();
 int length=Number1.Length;
 
 int[] array = new int[length];
+int k = Convert.ToInt32(Number1);
 int i=0;
-WriteLine($"длина {length}");
-WriteLine(Number1[2]);
 
-int k = int.Parse(Number1[1]);
-WriteLine(k);
+while (i<length) 
+{
+    array[i]=k%10;
+    k=k/10;
+    i++;
+}
 
+i=0;
+int b=length/2;
+bool w=true;
+
+while (i<b)
+{
+    if (array[i] != array[length-1-i]) 
+    {
+        Write("Введённое число не является палиндромом "); 
+        w=false; 
+        break;
+    } 
+    i++;
+} 
+if (w) WriteLine("Введенное число является палиндромом");
+WriteLine();
